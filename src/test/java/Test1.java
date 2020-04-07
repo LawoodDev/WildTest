84612780b35e903af8dfd743fe558a8edbb50124
LawoodDev <erwann.sale96@gmail.com>
2020-04-07 09:56:11 +0200

    Add os handler for chrome driver 80.0.3987.106

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,3 +1,5 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -10,19 +12,34 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElemen
 import java.time.Duration;
 
 public class Test1 {
+    WebDriver chrome;
+    WebDriverWait wait;
+
+    @BeforeEach
+    public void setup(){
+        ClassLoader classLoader = new Test1().getClass().getClassLoader();
+        String os = System.getProperty("os.name");
+        if((os.indexOf("win") >= 0)){
+            System.setProperty("webdriver.chrome.driver", classLoader.getResource("drivers/windows/chromedriver.exe").getPath());
+        }else if(os.indexOf("mac") >= 0){
+            System.setProperty("webdriver.chrome.driver", classLoader.getResource("drivers/mac/chromedriver").getPath());
+        }else if(os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0 || os.indexOf("aix") > 0 ){
+            System.setProperty("webdriver.chrome.driver", classLoader.getResource("drivers/linux/chromedriver").getPath());
+        }
+        chrome = new ChromeDriver();
+        wait = new WebDriverWait(chrome, Duration.ofSeconds(10));
+    }
 
     @Test
     public void searchCheese(){
-        System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         try {
-            driver.get("https://google.com");
-            driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
-            WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3>div")));
-            System.out.println(firstResult.getAttribute("textContent"));
+            chrome.get("https://google.com");
+            chrome.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+            WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
+            assertEquals("Cheese — Wikipédia",firstResult.getAttribute("textContent"));
         } finally {
-            driver.quit();
+            chrome.quit();
         }
     }
+
 }
